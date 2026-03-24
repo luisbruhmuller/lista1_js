ca26a3b4145155cd5b9406b6c2bc27398f36ecc4
luis <luisfelipe.bruhmuller@gmail.com>
2026-03-24 11:51:03 -0300

    ADD -exercicio 3 e 4

---
+++ scripts/ex06.js
@@ -0,0 +1,9 @@
+function ex06(){
+    let letra = "";
+    letra = (prompt("digite uma letra:"))
+    if (letra === "a","e","i","o","u"){
+        letra = "vogal";
+    }
+        letra = "consoante";
+    window.alert(letra)
+}

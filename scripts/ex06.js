345d0a791857af59e6f6fccbb6364282f8756651
luis <luisfelipe.bruhmuller@gmail.com>
2026-03-24 11:59:11 -0300

    ADD - exercicio 6

--- scripts/ex06.js
+++ scripts/ex06.js
@@ -1,9 +1,10 @@
 function ex06(){
     let letra = "";
     letra = (prompt("digite uma letra:"))
-    if (letra === "a","e","i","o","u"){
+    if (letra === "a" || letra === "e" || letra === "i" ||letra === "o" ||letra === "u"){
         letra = "vogal";
-    }
+    } else{
         letra = "consoante";
+    }
     window.alert(letra)
 }

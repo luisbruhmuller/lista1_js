ec174ac063b38648b1820d5ae21d7c7d10a9d125
luis <luisfelipe.bruhmuller@gmail.com>
2026-03-24 12:25:11 -0300

    ADD - exercicio 5

---
+++ scripts/ex05.js
@@ -0,0 +1,6 @@
+function ex05(){
+    let numero = 0;
+    numero = Number(prompt("digite um numero inteiro:"));
+    numero += 1;
+    window.alert(numero);
+}
---
+++ scripts/ex07.js
@@ -0,0 +1,12 @@
+function ex07(){
+    let a = ["Chocolote",1.50]
+    let b = ["Morango",2.50];
+    let c = ["Creme",2.50];
+    let d = ["Manga",3.20];
+    let e = ["Melancia",3.40];
+    let f = ["Vanilla Ice",3.00];
+    let g = ["Céu Azul",3.60];
+    let h = ["Brownie",4.00];
+    let i = ["Hawaiano",5.00];
+
+}

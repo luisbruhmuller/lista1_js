de33e2ba3a8f44dee015e705e66850df3fdf9016
luis <luisfelipe.bruhmuller@gmail.com>
2026-04-09 09:52:16 -0300

    ADD - exercicio 7 terminado

--- scripts/ex07.js
+++ scripts/ex07.js
@@ -1,14 +1,41 @@
 function ex07(){
-    let a = ["Chocolote",1.50]
-    let b = ["Morango",2.50];
-    let c = ["Creme",2.50];
-    let d = ["Manga",3.20];
-    let e = ["Melancia",3.40];
-    let f = ["Vanilla Ice",3.00];
-    let g = ["Céu Azul",3.60];
-    let h = ["Brownie",4.00];
-    let i = ["Hawaiano",5.00];
-    let resposta = "";
-    resposta = prompt("digite o codigo do picolé")
-    
+    var resposta
+    var a = [1, a, "Chocolote", 1.50];
+    var b = [2, b, "Morango", 2.50];
+    var c = [3, c, "Creme",  2.50];
+    var d = [4, d, "Manga", 3.20];
+    var e = [5, e, "Melancia", 3.40];
+    var f = [6, f, "Vanilla Ice", 3.00];
+    var g = [7, g, "Céu Azul", 3.60];
+    var h = [8, h, "Brownie", 4.00];
+    var i = [9, i, "Hawaiano", 5.00];
+    resposta = prompt("digite o codigo do picolé");
+   
+    if (resposta === "a")[
+        window.alert(a[2]+" "+ a[3] + " reais")
+    ]
+        if (resposta === "b")[
+        window.alert(b[2]+" "+ b[3] + " reais")
+    ]
+    if (resposta === "c")[
+        window.alert(c[2]+" "+ c[3] + " reais")
+    ]
+    if (resposta === "d")[
+        window.alert(d[2]+" "+ d[3] + " reais")
+    ]
+    if (resposta === "e")[
+        window.alert(e[2]+" "+ e[3] + " reais")
+    ]    
+    if (resposta === "f")[
+        window.alert(f[2]+" "+ f[3] + " reais")
+    ]
+    if (resposta === "g")[
+        window.alert(g[2]+" "+ g[3] + " reais")
+    ]
+    if (resposta === "h")[
+        window.alert(h[2]+" "+ h[3] + " reais")
+    ]
+    if (resposta === "i")[
+        window.alert(i[2]+" "+ i[3] + " reais")
+    ]
 }

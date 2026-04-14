bd4a5e776cc35fe89dcf53a1d5f8cecab26bdc85
luis <luisfelipe.bruhmuller@gmail.com>
2026-04-14 07:56:29 -0300

    ADD - exercicio 8

--- scripts/ex08.js
+++ scripts/ex08.js
@@ -1,3 +1,13 @@
-function ex08()[
+function ex08() {
+    let num1 = Number(prompt("Digite o primeiro número inteiro:"));
+    let num2 = Number(prompt("Digite o segundo número inteiro:"));
 
-]
+    let diferenca = num1 - num2;
+    console.log("A diferença entre os números é: " + diferenca);
+
+    let resultadoB = (num1 * 2) + (num2 * 3);
+    console.log("O dobro do primeiro + o triplo do segundo é: " + resultadoB);
+
+    let multiplicacao = num1 * num2;
+    console.log("A multiplicação dos dois números é: " + multiplicacao);
+}
